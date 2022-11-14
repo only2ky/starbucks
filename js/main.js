6ebbfaf36f94994f24d2bb3da936cd5fcd649747
only2ky <only2ky@naver.com>
2022-11-14 18:54:12 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchinputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchinputEl.focus();
-});
-
-searchinputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchinputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchinputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchinputEl.setAttribute('placeholder', '');
-});
 
 const badgeEl =document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
